0602c044b98405bbb5192a0dac57257f39b7d5b4
aldofelipe <aldobsilva70@gmail.com>
2023-05-27 14:44:34 -0300

    comunicação do controle com a IA

---
+++ protocolo_comunicacao.cpp
@@ -0,0 +1,28 @@
+Gaveta aberta = G0
+Gaveta fechada = G1
+
+Enviar toda vez que entrar uma pilha nova:
+{
+Pilha AAA Cheio = AAA1
+Pilha AAA Vazio = AAA0
+
+Pilha AA Cheio = AA1
+Pilha AA Vazio = AA0
+
+Pilha 9V Cheio = 9V1
+Pilha 9V Vazio = 9V0
+
+Pilha C Cheio = C1
+Pilha C Vazio = C0
+
+Pilha D Cheio = D1
+Pilha D Vazio = D0}
+
+Analisar Imagem = AI
+
+AI0 = 9V
+AI1 = AA
+AI2 = AAA
+AI3 = C
+AI4 = D
+AI5 = UNKWON
--- teste_sensores_pigpio.cpp
+++ teste_sensores_pigpio.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <pigpio.h>
+#include <pigpiod_if2.h>
 
 // sensor magnético
 #define HALL_SENSOR_PIN 17 // GPIO 17
@@ -11,51 +11,52 @@
 #define IR_SENSOR_PIN_5 16 // GPIO 16
 
 int main() {
-    if (gpioInitialise() < 0) {
-        std::cerr << "Falha ao inicializar o pigpio" << std::endl << std::flush;
+    int pi = pigpio_start(NULL, NULL);
+    if (pi < 0) {
+        std::cerr << "Falha ao inicializar o pigpio" << std::endl;
         return 1;
     }
 
-    gpioSetMode(HALL_SENSOR_PIN, PI_INPUT);
+    set_mode(pi, HALL_SENSOR_PIN, PI_INPUT);
 
     while (true) {
-        if (gpioRead(HALL_SENSOR_PIN) == PI_LOW) {
+        if (gpio_read(pi, HALL_SENSOR_PIN) == 0) {
             std::cout << "Gaveta fechada" << std::endl;
         } else {
             std::cout << "Gaveta aberta" << std::endl;
         }
 
-        if (gpioRead(IR_SENSOR_PIN_1) == PI_LOW) {
-            std::cout << "Compartimento da pilha AAA não está vazio" << std::endl << std::flush;
+        if (gpio_read(pi, IR_SENSOR_PIN_1) == 0) {
+            std::cout << "Compartimento da pilha AAA não está vazio" << std::endl;
         } else {
-            std::cout << "Compartimento da pilha AAA está vazio" << std::endl << std::flush;
+            std::cout << "Compartimento da pilha AAA está vazio" << std::endl;
         }
 
-        if (gpioRead(IR_SENSOR_PIN_2) == PI_LOW) {
-            std::cout << "Compartimento da pilha AA não está vazio" << std::endl << std::flush;
+        if (gpio_read(pi, IR_SENSOR_PIN_2) == 0) {
+            std::cout << "Compartimento da pilha AA não está vazio" << std::endl;
         } else {
-            std::cout << "Compartimento da pilha AA está vazio" << std::endl << std::flush;
+            std::cout << "Compartimento da pilha AA está vazio" << std::endl;
         }
 
-        if (gpioRead(IR_SENSOR_PIN_3) == PI_LOW) {
-            std::cout << "Compartimento da pilha C não está vazio" << std::endl << std::flush;
+        if (gpio_read(pi, IR_SENSOR_PIN_3) == 0) {
+            std::cout << "Compartimento da pilha C não está vazio" << std::endl;
         } else {
-            std::cout << "Compartimento da pilha C está vazio" << std::endl << std::flush;
+            std::cout << "Compartimento da pilha C está vazio" << std::endl;
         }
 
-        if (gpioRead(IR_SENSOR_PIN_4) == PI_LOW) {
-            std::cout << "Compartimento da pilha D não está vazio" << std::endl << std::flush;
+        if (gpio_read(pi, IR_SENSOR_PIN_4) == 0) {
+            std::cout << "Compartimento da pilha D não está vazio" << std::endl;
         } else {
-            std::cout << "Compartimento da pilha D está vazio" << std::endl << std::flush;
+            std::cout << "Compartimento da pilha D está vazio" << std::endl;
         }
 
-        if (gpioRead(IR_SENSOR_PIN_5) == PI_LOW) {
-            std::cout << "Compartimento da pilha 9V não está vazio" << std::endl << std::flush;
+        if (gpio_read(pi, IR_SENSOR_PIN_5) == 0) {
+            std::cout << "Compartimento da pilha 9V não está vazio" << std::endl;
         } else {
-            std::cout << "Compartimento da pilha 9V está vazio" << std::endl << std::flush;
+            std::cout << "Compartimento da pilha 9V está vazio" << std::endl;
         }
     }
 
-    gpioTerminate();
+    pigpio_stop(pi);
     return 0;
 }
