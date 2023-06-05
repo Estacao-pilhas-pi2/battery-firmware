ab344e77d1241bf67a84387a6ccbf24718a4703c
Lukas Araújo <109924908+lukas-araujo@users.noreply.github.com>
2023-06-05 09:46:07 -0300

    Update and rename teste_sensores_pigpio.cpp to teste_comun.cpp
    começar integração com motores

--- teste_comun.cpp
+++ teste_comun.cpp
@@ -3,12 +3,18 @@
 
 // sensor magnético
 #define HALL_SENSOR_PIN 17 // GPIO 17
-// sensores de obstáculos 
+
+
+
+// sensores das gavetas 
 #define IR_SENSOR_PIN_1 23 // GPIO 23
 #define IR_SENSOR_PIN_2 24 // GPIO 24
 #define IR_SENSOR_PIN_3 25 // GPIO 25
 #define IR_SENSOR_PIN_4 8  // GPIO 8
 #define IR_SENSOR_PIN_5 16 // GPIO 16
+
+// sensor da entrada
+#define IR_SENSOR_PIN_6 5 // GPIO 5
 
 int main() {
     int pi = pigpio_start(NULL, NULL);
@@ -18,6 +24,11 @@ int main() {
     }
 
     set_mode(pi, HALL_SENSOR_PIN, PI_INPUT);
+    set_mode(pi, HALL_SENSOR_PIN_1, PI_INPUT);
+    set_mode(pi, HALL_SENSOR_PIN_2, PI_INPUT);
+    set_mode(pi, HALL_SENSOR_PIN_3, PI_INPUT);
+    set_mode(pi, HALL_SENSOR_PIN_4, PI_INPUT);
+    set_mode(pi, HALL_SENSOR_PIN_5, PI_INPUT);
 
     while (true) {
         if (gpio_read(pi, HALL_SENSOR_PIN) == 0) {
